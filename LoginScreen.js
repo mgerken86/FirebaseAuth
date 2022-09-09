2607b3b8e48e36160fab0c828af500e406fe77d4
Mark Gerken <mgerken86@gmail.com>
2022-09-09 14:38:27 -0600

    textInputs

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { TextInput } from 'react-native-gesture-handler'
 
 const LoginScreen = () => {
   return (
-    <View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Email'
+          // value={}
+          // onChangeText={text => }
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Password'
+          // value={}
+          // onChangeText={text => }
+          style={styles.input}
+          secureTextEntry
+        />
+
+      </View>
       <Text>Login Screen</Text>
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
