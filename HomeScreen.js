8644a62b572fd462c03da73751b6c63f0dab1ef9
Mark Gerken <mgerken86@gmail.com>
2022-09-10 16:33:12 -0600

    handleSignOut

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { auth } from './firebase'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace('Login')
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Email</Text>
+      <Text>Email: {auth.currentUser?.email}</Text>
       <TouchableOpacity
+      onPress={handleSignOut}
       style={styles.button}
       >
         <Text style={styles.buttonText}>Sign out</Text>
@@ -25,26 +38,16 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#0782F9',
-    width: '100%',
+    width: '60%',
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 40,
   },
   buttonText: {
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
 
-  },
-  buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782F9',
-    borderWidth: 2
-  },
-  buttonOutlineText: {
-    color: '#0782F9',
-    fontWeight: '700',
-    fontSize: 16,
   },
 })
